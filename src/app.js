5c213809aa78430bb87a1cff500548665cd31797
Jessica Grinberg <jessgrinberg@gmail.com>
2016-03-08 18:39:03 -0800

    set up express API

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ var app = express();
 
 app.use('/', express.static('public'));
 
+var router = express.Router();
+
+router.get('/todos', function(req, res) {
+  res.json({todos: []});
+});
+
+//TODO: Add POST route to create new entries
+
+//TODO: Add PUT route to update existing entries
+
+//TODO: Add DELETE route to delete entries
+
+
+app.use('/api', router);
+
 
 app.listen(3000, function() {
   console.log("The serving is running on port 3000!")
